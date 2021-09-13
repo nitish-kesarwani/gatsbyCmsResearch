8720f6742c2ddd0fba2b13a5d6b7c3d492aa20d2
Nitish Kesarwani <Nitish.Kesarwani@rsystems.com>
2021-09-14 00:29:38 +0530

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,76 @@
 import * as React from "react"
+import {useState, useEffect} from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faAngleDown
+} from '@fortawesome/free-solid-svg-icons'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+
+const Header = ({ siteTitle }) => {	
+
+	return (
+		<>
+			<header
+				style={{
+					background: `#00375e`,
+					padding:'0.7rem'
+				}}
+			>
+				<div style={{textAlign:'center'}}>
+					<span style={{color:'#fff'}}>Get your copy of the</span>
+					<span style={{color:'#55a4da'}}>&nbsp;
+						2020 Gartner Market Guide for Digital Asset Management
+					</span>
+					<span className='accessBtn'>Access Now</span>
+				</div>
+			</header>
+			<FixHeader/>
+		</>
+	)
+}
+
+const FixHeader = () => {
+	
+	const [fix, setstate] = useState(false)
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			if(window?.scrollY > 52){
+				setstate(true)
+			}else{
+				setstate(false)
+			}
+		});
+	}, []); 
+	
+	return(
+		<div className={`${fix ? 'fix fixHeader' : 'fixHeader'}`}>
+			<div style={{maxWidth:1140, margin:'auto', display:'flex', alignItems:'center'}}>
+				<StaticImage
+					src="../images/logo.png"
+					width={252}
+					quality={95}
+					formats={["auto", "webp", "avif"]}
+					alt="A Gatsby astronaut"
+				/>
+				<nav className='navBar'>
+					<a href="/html/">Products <FontAwesomeIcon icon={faAngleDown} size="1x" /></a>
+					<a href="/css/">Solutions <FontAwesomeIcon icon={faAngleDown} size="1x" /></a>
+					<a href="/js/">Resources <FontAwesomeIcon icon={faAngleDown} size="1x" /></a>
+					<a href="/python/">Partners <FontAwesomeIcon icon={faAngleDown} size="1x" /></a>
+					<a href="/python/">About Us <FontAwesomeIcon icon={faAngleDown} size="1x" /></a>
+				</nav>
+				<span className='accessBtn'>Contact Us</span>
+				<span className='accessBtn success'>Get Free Demo</span>
+			</div>
+		</div>
+	)
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+	siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,119 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+  	<Layout>
+    	<Seo title="Home" />
+		<section className='container' style={{
+			background: 'transparent url(https://mediavalet-corp.azureedge.net/wp-content/uploads/2019/01/MV-Homepage-Artificial-Intelligence-Hero-header-Jan2019.png) no-repeat center center',
+			backgroundSize: 'contain',
+			padding:'110px 0',
+			backgroundPosition:'25vw bottom'
+		}}
+    >
+      	<div style={{width:'50%'}}>
+			<h1 style={{color:'#2483c5', fontSize:'3.1rem', fontFamily:'Roboto,sans-serif', fontWeight:300, lineHeight:1.2}}>
+				Enterprise Digital Asset
+				Management in the Cloud
+			</h1>
+			<h3 style={{fontSize:'2.1rem', fontWeight:300, margin:'2rem 0'}}>
+				Discover the 
+				<em style={{color:'#98c43e', padding:'0 10px', fontFamily:'house-script,Times New Roman,Times,serif'}}>power</em>
+				of your assets
+			</h3>
+        	<span className='accessBtn' style={{marginLeft:0, fontSize:'1.3rem', padding:'8px 13px'}}>Get Free Demo</span>
+      	</div>
+      	<div style={{width:'50%'}}></div>
+    </section>
+	<section className='container' style={{
+			display:'flex',
+			justifyContent:'space-between',
+			alignItems:'center',
+			padding:'65px 0'
+		}}
+	>
+			<StaticImage
+				src={`../images/1.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/2.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/3.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/4.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/5.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/6.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/7.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/8.png`}
+				width={105}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+	</section>
+	<section className='gradient-background' style={{
+		padding:'65px 0'
+	}}>
+		<div className='container' style={{
+			display:'flex',
+			justifyContent:'space-between',
+			alignItems:'center'
+		}}>
+			<StaticImage
+				src={`../images/title1.jpg`}
+				width={348}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/title2.jpg`}
+				width={348}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+			<StaticImage
+				src={`../images/title3.jpg`}
+				width={348}
+				quality={100}
+				formats={["auto", "webp", "avif"]}
+			/>
+		</div>
+	</section>
+    {/* <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    </p> */}
   </Layout>
 )
 
